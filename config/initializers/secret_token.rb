dfc58b54a7ed5ad077b2d5b8c0bf80c10bd09e8f
Josh Bryans <jbryans@rails.(none)>
2013-08-20 00:47:10 +0000

    Rename Readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7460bca8840f4a91fdb40292cae90848b444be5f44afb18927486bedfc45b06720689b0116ef9ada6db5331005b3abe717642b6ab71642f57c38afb8896ae7b8'
